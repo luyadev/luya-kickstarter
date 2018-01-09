0a608d0ee5d24915946768caf21b51d9768fc787
Silvan <silvan@dev7.ch>
2018-01-09 17:28:08 +0100

    remove import info after install

--- messages/vi/app.php
+++ messages/vi/app.php
@@ -2,7 +2,6 @@
 
 return [
     'kickstarter_success' => '<h1 class="alert-heading">Cài đặt thành công!</h1>
-    <p>Bạn đã hoàn tất việc cài đặt <i>LUYA</i> trên máy của bạn. Ngay bây giờ bạn đã có thể truy cập vào trang quản trị.</p>
-    <p><strong>Lưu ý</strong><br />Sau khi đăng nhập vào hệ thống bạn hãy truy cập vào menu [Hệ thống] để thiết lập quyền hạn truy xuất các module của nhóm Adminstrator.</p>',
+    <p>Bạn đã hoàn tất việc cài đặt <i>LUYA</i> trên máy của bạn. Ngay bây giờ bạn đã có thể truy cập vào trang quản trị.</p>',
     'kickstarter_admin_link' => '<p><a href="{link}" class="btn btn-primary btn-lg">Vào trang quản trị</a></p>',
 ];
