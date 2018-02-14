8109194c77802bdb4e1734f7dc643b5e78d6c020
Silvan <silvan@dev7.ch>
2018-02-14 19:15:18 +0100

    remove double og title and description

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -17,9 +17,7 @@ $this->beginPage();
         <meta name="robots" content="index, follow" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta property="og:title" content="LUYA - Build any system" />
         <meta property="og:image" content="<?= $this->publicHtml ?>/images/logo/2x/luya_logo@2x-100.jpg" />
-        <meta property="og:type" content="website"/>
         <link rel="apple-touch-icon" sizes="180x180" href="<?= $this->publicHtml ?>/favicon/apple-touch-icon.png">
         <link rel="icon" type="image/png" sizes="32x32" href="<?= $this->publicHtml ?>/favicon/favicon-32x32.png">
         <link rel="icon" type="image/png" sizes="16x16" href="<?= $this->publicHtml ?>/favicon/favicon-16x16.png">
