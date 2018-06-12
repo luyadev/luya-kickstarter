96c0bd8772162b8fa74ce960590a74ecd440a2ee
Alain Rollmann <arollmann@gmail.com>
2018-06-12 10:00:06 +0200

    Update app.php (#27)

--- messages/fr/app.php
+++ messages/fr/app.php
@@ -1,6 +1,6 @@
 <?php
 return [
-  'kickstarter_success' => '<h1 class="alert-heading">L\'installation s\'est terminée avec succès!</h1>
+  'kickstarter_success' => '<h1 class="alert-heading">L\'installation s\'est terminée avec succès !</h1>
   <p<i>LUYA</i> est à présent installé sur votre serveur. Pour créer vos pages, connectez-vous à la zone d\'administration.</p>',
   'kickstarter_admin_link' => '<p><a href="{link}" class="btn btn-primary btn-lg">Accéder à la zone d\'administration</a></p>',
 ];
