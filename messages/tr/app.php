573ac9c88ec75624846258def26cedff52b4b0f2
Iskender TOTOGLU <totoglu@gmail.com>
2018-06-14 18:03:23 +0300

    Turkish messages

---
+++ messages/tr/app.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'kickstarter_success' => '<h1 class="alert-heading">Hazırsınız!</h1>
+    <p>Evet, <i>LUYA</i>\'yı sisteminize başarıyla yüklediniz. Sayfalarınızı oluşturmak için yönetim panelinize giriş yapabilirsiniz.</p>',
+    'kickstarter_admin_link' => '<p><a href="{link}" class="btn btn-primary btn-lg">Yönetim panelini aç</a></p>',
+];
